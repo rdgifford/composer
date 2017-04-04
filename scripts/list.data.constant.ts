534cfe5b0cb2053a7f6796499f40eb5f0c0ad84a
Guy <guythomas721@gmail.com>
2017-04-04 22:58:33 +1000

    Cut out the fat

--- scripts/list.data.constant.ts
+++
@@ -1,6 +0,0 @@
-export const BOOKMARKS = [
-  { 'name': 'Twitter', 'url': 'https://twitter.com' },
-  { 'name': 'Github', 'url': 'https://github.com' },
-  { 'name': 'Sitepoint', 'url': 'https://sitepoint.com' },
-  { 'name': 'Codepen', 'url': 'https://codepen.com' }
-];
