c9df7749312bf71e6ed4e9aa2907fba5c3d807e3
Guy <guythomas721@gmail.com>
2017-04-04 22:54:52 +1000

    Completed MVP

--- js/emailInject.js
+++ js/emailInject.js
@@ -1,6 +1,6 @@
 const $ = jQuery;
 const createEmail = {
-	body: function (name, role) {
+	body: function (name, role, company) {
 		const createIntro = function (name) {
 			if (name) {
 				return 'Hi ' + name + ','
@@ -11,7 +11,7 @@ const createEmail = {
 
 		const body = `${createIntro(name)}
 
-		My name is Guy and I submitted my resume for a ${role}. I’m not sure who the hiring manager is for this position but I'm extremely interested in the opportunity, and I just wanted to touch base to see if there’s anything that I could provide to help show my fit for the role and perhaps even get in touch with the hiring manager of the position.
+		My name is Guy and I submitted my resume for a ${role} at ${company}. I’m not sure who the hiring manager is for this position but I'm extremely interested in the opportunity, and I just wanted to touch base to see if there’s anything that I could provide to help show my fit for the role and perhaps even get in touch with the hiring manager of the position.
 
 		Thank you for your time and kind consideration.
 
@@ -28,7 +28,7 @@ const createEmail = {
 		return link;
 	},
 	subject: function() {
-		return 'Dummy subject'
+		return 'Full Stack Developer Application'
 	}
 }
 
@@ -38,6 +38,7 @@ function emailWrapper() {
 	const $emails = $("div").filter(function () {
 	    return validEmail.test($(this).text()); 
 	});
+	const company = $('.company-info-name').text();
 
 	//Extract data from sibing divs
 	$emails.each((index, item) => {
@@ -53,20 +54,18 @@ function emailWrapper() {
 		
 		//Convert to data for the required format in the email link
 		const firstName = firstLastName[0];
-		const body = createEmail.body(firstName, 'Full Stack Developer');
-		const link = createEmail.link('replace-with-email-variable', 'Full Stack Web Devleoper Application', body)
+		const body = createEmail.body(firstName, 'Full Stack Developer', company);
+		const link = createEmail.link(email, 'Full Stack Devleoper Application', body)
 		$(item).wrap( `<a href=${link} target=_blank></a>` );
 	})
 	$emails.css( "text-decoration", "underline" );
 }
 
 chrome.runtime.sendMessage({}, function(response) {
-	console.log('sending message')
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 		emailWrapper();
-		console.log("Sendmessage in document ready complete");
 	}
 	}, 10);
 });
